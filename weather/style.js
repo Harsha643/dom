9aaa3be60414b290e43f3eded8ac086506c844b3
Harsha643 <battuharsha6@gmail.com>
2025-01-06 22:29:38 +0530

    harsha

--- weather/style.js
+++ weather/style.js
@@ -1,21 +1,43 @@
-// const apiKey = 'c4f86ece00bc8aa272652ac9065af12d';
-// const url = `https://api.openweathermap.org/data/2.5/weather?q=india&appid=${apiKey}&units=metric`; 
+// // const apiKey = 'c4f86ece00bc8aa272652ac9065af12d';
+// // const url = `https://api.openweathermap.org/data/2.5/weather?q=india&appid=${apiKey}&units=metric`; 
 
-// async function _name(url) {
-//     try {
-//         const response = await fetch(url);
-//         const result = await response.json();
-//         console.log(result);
-//         console.log(result.weather[0].description)
-//     } catch (error) {
-//         console.error(error);
-//     }
-// }
-// _name(url)
+// // async function _name(url) {
+// //     try {
+// //         const response = await fetch(url);
+// //         const result = await response.json();
+// //         console.log(result);
+// //         console.log(result.weather[0].description)
+// //     } catch (error) {
+// //         console.error(error);
+// //     }
+// // }
+// // _name(url)
 
 
 
-// const url = `https://open-weather13.p.rapidapi.com/city/pune/EN`;
+// // const url = `https://open-weather13.p.rapidapi.com/city/pune/EN`;
+// // const options = {
+// // 	method: 'GET',
+// // 	headers: {
+// // 		'x-rapidapi-key': 'ae42d6fe5bmshc4c1462b3b6aa15p1b7dffjsn266ee5e64111',
+// // 		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+// // 	}
+// // };
+// // async function _name() {
+    
+// // try {
+// // 	const response = await fetch(url, options);
+// // 	const result = await response.json();
+// // 	console.log(result);
+// // } catch (error) {
+// // 	console.error(error);
+// // }
+// // }
+// // _name()
+
+
+// let cityname=prompt("enter city name")
+// const url = `https://open-weather13.p.rapidapi.com/city/${cityname}/EN`;
 // const options = {
 // 	method: 'GET',
 // 	headers: {
@@ -23,42 +45,18 @@
 // 		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
 // 	}
 // };
-// async function _name() {
-    
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.json();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
-// }
-// }
-// _name()
-
-
-let cityname=prompt("enter city name")
-const url = `https://open-weather13.p.rapidapi.com/city/${cityname}/EN`;
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'ae42d6fe5bmshc4c1462b3b6aa15p1b7dffjsn266ee5e64111',
-		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
-	}
-};
 
 
 
 async function _name() {
+
+try {
 	const response = await fetch(url, options);
 	const result = await response.json();
 	weather(result)
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.json();
-// 	weather(result)
-// } catch (error) {
-// 	console.error(error);
-// }
+} catch (error) {
+	console.error(error);
+}
 }
 _name()
 
